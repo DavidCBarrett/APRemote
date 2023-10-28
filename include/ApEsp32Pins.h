e0ea2943bc51d78989eebfac86dd994dbafd6248
David B <dbarrett91@hotmail.com>
2023-10-28 19:10:09 +0100

    Tidied & added beep & TFT brightness pins

--- include/ApEsp32Pins.h
+++ include/ApEsp32Pins.h
@@ -3,17 +3,20 @@
 
 #include <Arduino.h>
 
-// Software Serial pins config (oh and the LED pin) Pins are GPIO numbers, not ESP32 module pin no's 
-#define RX_LED          32
-#define TX_LED          33
-#define LED_PIN         2
+// Software Serial pins config. Pins are GPIO numbers, not ESP32 module pin no's 
+#define LED_PIN         GPIO_NUM_2
 
 // The max no of bytes expected in a SeaTalk command 
 #define MAX_BUF_SIZE    20
 
 // Software Serial pins config Pins are GPIO numbers, not ESP32 module pin no's 
-#define RX_IN           25
-#define RX_MON          26
-#define TX_OUT          27
+#define RX_IN           GPIO_NUM_25
+#define RX_MON          GPIO_NUM_26
+#define TX_OUT          GPIO_NUM_27
+
+#define BEEPERPIN          GPIO_NUM_4
+#define TFTBACKLIGHTPIN    GPIO_NUM_16
+
+// TFT Pins are in platformio.ini
 
 #endif // end _APESP32PINS_H
